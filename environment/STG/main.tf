231fa5c8dbd0758277fc0bb196067c40e1af6627
ibrahim <ibrahim@alnagar.com>
2026-04-28 12:19:10 +0300

    Edit the architecture

---
+++ environment/STG/main.tf
@@ -0,0 +1,48 @@
+terraform {
+  required_providers {
+
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+#Default provider
+provider "azurerm" {
+
+  features {}
+  tenant_id       = var.IB_Tenant_id
+  subscription_id = var.IB_Sub_STG
+  client_id       = var.IB_Sub_STG_appid
+  client_secret   = var.IB_Sub_STG_st
+}
+
+provider "azurerm" { 
+  alias = "SUb-TST"
+  features {}
+  tenant_id       = var.IB_Tenant_id
+  subscription_id = var.IB_Sub_TST
+  client_id       = var.IB_Sub_TST_appid
+  client_secret   = var.IB_Sub_TST_st
+}
+
+#Default provider
+provider "azurerm" {
+  alias = "Sub-STG"
+  features {}
+  tenant_id       = var.IB_Tenant_id
+  subscription_id = var.IB_Sub_STG
+  client_id       = var.IB_Sub_STG_appid
+  client_secret   = var.IB_Sub_STG_st
+}
+
+
+provider "azurerm" {
+  alias = "SUb-PRD"
+  features {}
+  tenant_id       = var.IB_Tenant_id
+  subscription_id = var.IB_Sub_PRD
+  client_id       = var.IB_Sub_PRD_appid
+  client_secret   = var.IB_Sub_PRD_st
+}
---
+++ environment/STG/myvariables.tf
@@ -0,0 +1,18 @@
+variable "IB_Tenant_id" {}
+
+variable "IB_Sub_TST" {}
+variable "IB_Sub_TST_appid" {}
+variable "IB_Sub_TST_st" {}
+
+variable "IB_Sub_STG" {}
+variable "IB_Sub_STG_appid" {}
+variable "IB_Sub_STG_st" {}
+
+
+
+variable "IB_Sub_PRD" {}
+variable "IB_Sub_PRD_appid" {}
+variable "IB_Sub_PRD_st" {}
+
+variable "SH_VM_Pass" {}
+variable "SH_VM_User" {}
