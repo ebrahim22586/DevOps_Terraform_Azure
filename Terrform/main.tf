651e5b6b6e482d1fdfc444110ffc9753610edcde
ibrahim <ibrahim@alnagar.com>
2026-04-28 12:14:41 +0300

    Edit variables file

--- Terrform/main.tf
+++ Terrform/main.tf
@@ -12,37 +12,37 @@ terraform {
 provider "azurerm" {
 
   features {}
-  tenant_id       = var.IB-Tenant-id
-  subscription_id = var.IB-Sub-STG
-  client_id       = var.IB-Sub-STG_appid
-  client_secret   = var.IB-Sub-STG_st
+  tenant_id       = var.IB_Tenant_id
+  subscription_id = var.IB_Sub_STG
+  client_id       = var.IB_Sub_STG_appid
+  client_secret   = var.IB_Sub_STG_st
 }
 
 provider "azurerm" { 
   alias = "SUb-TST"
   features {}
-  tenant_id       = var.IB-Tenant-id
-  subscription_id = var.IB-Sub-TST
-  client_id       = var.IB-Sub-TST_appid
-  client_secret   = var.IB-Sub-TST_st
+  tenant_id       = var.IB_Tenant_id
+  subscription_id = var.IB_Sub_TST
+  client_id       = var.IB_Sub_TST_appid
+  client_secret   = var.IB_Sub_TST_st
 }
 
 #Default provider
 provider "azurerm" {
   alias = "Sub-STG"
   features {}
-  tenant_id       = var.IB-Tenant-id
-  subscription_id = var.IB-Sub-STG
-  client_id       = var.IB-Sub-STG_appid
-  client_secret   = var.IB-Sub-STG_st
+  tenant_id       = var.IB_Tenant_id
+  subscription_id = var.IB_Sub_STG
+  client_id       = var.IB_Sub_STG_appid
+  client_secret   = var.IB_Sub_STG_st
 }
 
 
 provider "azurerm" {
   alias = "SUb-PRD"
   features {}
-  tenant_id       = var.IB-Tenant-id
-  subscription_id = var.IB-Sub-PRD
-  client_id       = var.IB-Sub-PRD_appid
-  client_secret   = var.IB-Sub-PRD_st
+  tenant_id       = var.IB_Tenant_id
+  subscription_id = var.IB_Sub_PRD
+  client_id       = var.IB_Sub_PRD_appid
+  client_secret   = var.IB_Sub_PRD_st
 }
