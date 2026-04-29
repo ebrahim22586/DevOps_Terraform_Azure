0ff53b055a5f9782e8a09b499d1f0d5756cab6d7
ibrahim <ibrahim@alnagar.com>
2026-04-29 14:45:00 +0300

    edit con name on backend tf

--- terraform/STG/backecnd.tf
+++ terraform/STG/backecnd.tf
@@ -6,8 +6,8 @@ terraform {
     tenant_id            = "59d38026-3cd2-4194-8eb6-e56ee9eeffcd"
     subscription_id      = "6fffa98c-3cf6-4729-bb78-3e4ae94eefaf"
     storage_account_name = "backendsaterrazure"
-    container_name       = "STGtfstate"
-    key                  = "STG/fstate"
+    container_name       = "stgtfstate"
+    key                  = "stg/fstate"
     use_azuread_auth     = true
     use_oidc  = true
     client_id = "47b8a8ab-12d4-43e1-a567-62de87b2e2cc"  # ← This is likely missing
