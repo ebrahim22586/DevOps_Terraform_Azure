883e26f378debcf27fe009caa645da73674e77b3
ibrahim <ibrahim@alnagar.com>
2026-04-30 10:45:44 +0300

    push state into a new sa

--- terraform/STG/backecnd.tf
+++ terraform/STG/backecnd.tf
@@ -6,7 +6,7 @@ terraform {
     tenant_id            = "59d38026-3cd2-4194-8eb6-e56ee9eeffcd"
     subscription_id      = "6fffa98c-3cf6-4729-bb78-3e4ae94eefaf"
     storage_account_name = "backendsaterrazure"
-    container_name       = "stgtfstate"
+    container_name       = "stgtfstate2"
     key                  = "stg/fstate"
     use_azuread_auth     = true
     use_oidc  = true
